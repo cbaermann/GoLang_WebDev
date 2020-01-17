b1d8cb79c0f6a8fcdcd6d537144f53e24da54a48
Cody Baermann <cbaermann@comcast.net>
2020-01-17 07:07:33 -0800

    program adds image to cookie range list over html page

--- photoBlog/blog/main.go
+++ photoBlog/blog/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"crypto/sha1"
+	"fmt"
+	"html/template"
+	"io"
 	"net/http"
-	"text/template"
+	"os"
+	"path/filepath"
+	"strings"
 
 	uuid "github.com/satori/go.uuid"
 )
@@ -15,24 +21,60 @@ func init() {
 
 func main() {
 	http.HandleFunc("/", index)
-	http.Handle("favicon.ico", http.NotFoundHandler())
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	c := getCookie(w, r)
-	tpl.ExecuteTemplate(w, "index.html", c.Value)
+	//form submission
+	if r.Method == http.MethodPost {
+		mf, fh, err := r.FormFile("nf")
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer mf.Close()
+		ext := strings.Split(fh.Filename, ".")[1]
+		h := sha1.New()
+		io.Copy(h, mf)
+		fname := fmt.Sprintf("%x", h.Sum(nil)) + "." + ext
+		wd, err := os.Getwd()
+		if err != nil {
+			fmt.Println(err)
+		}
+		path := filepath.Join(wd, "public", "pics", fname)
+		nf, err := os.Create(path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer nf.Close()
+		mf.Seek(0, 0)
+		io.Copy(nf, mf)
+		c = appendValue(w, c, fname)
+	}
+	xs := strings.Split(c.Value, "|")
+	tpl.ExecuteTemplate(w, "index.html", xs)
 }
 
 func getCookie(w http.ResponseWriter, r *http.Request) *http.Cookie {
 	c, err := r.Cookie("session")
 	if err != nil {
 		sID, _ := uuid.NewV4()
-		c := &http.Cookie{
+		c = &http.Cookie{
 			Name:  "session",
 			Value: sID.String(),
 		}
 		http.SetCookie(w, c)
 	}
+	return c
+}
+
+func appendValue(w http.ResponseWriter, c *http.Cookie, fname string) *http.Cookie {
+	s := c.Value
+	if !strings.Contains(s, fname) {
+		s += "|" + fname
+	}
+	c.Value = s
+	http.SetCookie(w, c)
 	return c
 }
