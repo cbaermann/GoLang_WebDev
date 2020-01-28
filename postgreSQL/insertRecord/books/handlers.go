8b41c595749b32558d33264d74ea353e95abab6a
Cody Baermann <cbaermann@comcast.net>
2020-01-28 05:20:06 -0800

    refactored code

---
+++ postgreSQL/insertRecord/books/handlers.go
@@ -0,0 +1,110 @@
+package books
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/GoesToEleven/golang-web-dev/045-code-organization/03_multiple-packages/config"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	bks, err := AllBooks()
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	config.TPL.ExecuteTemplate(w, "books.html", bks)
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	bk, err := OneBook(r)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	config.TPL.ExecuteTemplate(w, "show.html", bk)
+}
+
+func Create(w http.ResponseWriter, r *http.Request) {
+	config.TPL.ExecuteTemplate(w, "create.html", nil)
+}
+
+func CreateProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	bk, err := PutBook(r)
+	if err != nil {
+		http.Error(w, http.StatusText(406), http.StatusNotAcceptable)
+		return
+	}
+
+	config.TPL.ExecuteTemplate(w, "created.html", bk)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	bk, err := OneBook(r)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	config.TPL.ExecuteTemplate(w, "update.html", bk)
+}
+
+func UpdateProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	bk, err := UpdateBook(r)
+	if err != nil {
+		http.Error(w, http.StatusText(406), http.StatusBadRequest)
+		return
+	}
+
+	config.TPL.ExecuteTemplate(w, "updated.html", bk)
+}
+
+func DeleteProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := DeleteBook(r)
+	if err != nil {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	http.Redirect(w, r, "/books", http.StatusSeeOther)
+}
