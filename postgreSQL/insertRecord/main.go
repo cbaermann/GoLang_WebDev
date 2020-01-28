40db7d79887f6bec93e4f0187d367034214248f7
Cody Baermann <cbaermann@comcast.net>
2020-01-28 05:09:19 -0800

    delete method added, full CRUD in place

--- postgreSQL/insertRecord/main.go
+++ postgreSQL/insertRecord/main.go
@@ -42,6 +42,9 @@ func main() {
 	http.HandleFunc("/books/show", booksShow)
 	http.HandleFunc("/books/create", booksCreateForm)
 	http.HandleFunc("/books/create/process", booksCreateProcess)
+	http.HandleFunc("/books/update", booksUpdateForm)
+	http.HandleFunc("/books/update/process", booksUpdateProcess)
+	http.HandleFunc("/books/delete/process", booksDeleteProcess)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -213,4 +216,25 @@ func booksUpdateProcess(w http.ResponseWriter, r *http.Request) {
 	// confirm insertion
 	tpl.ExecuteTemplate(w, "updated.html", bk)
 
+}
+
+func booksDeleteProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+	//delete book
+
+	_, err := db.Exec("DELETE FROM books WHERE isbn=$1;", isbn)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, "/books", http.StatusSeeOther)
 }
